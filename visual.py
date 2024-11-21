0d68db9760f17bd9272020fe416c6e9afe8d00a5
6643789wsx <qaz6643789wsx@163.com>
2024-11-21 16:46:06 +0800

    feature: modify header
    人们眼中的天才之所以卓越非凡，并非天资超人一等，而是付出了持续不断的努力。1万小时的锤炼是任何人从平凡变成世界级大师的必要条件。

--- visual.py
+++ visual.py
@@ -30,25 +30,41 @@ def read_log_files(directory):
 def summarize_daily_durations(data):
     df = pd.DataFrame(data, columns=['date', 'duration'])
     daily_summary = df.groupby('date')['duration'].sum()
-    daily_summary_hours = daily_summary.round(3)  # Round to three decimal places
+    daily_summary_hours = daily_summary.round(1)  # Round to one decimal place
     daily_summary_hms = daily_summary.apply(lambda x: str(timedelta(hours=x)))
     summary_df = pd.DataFrame({
         'Date': daily_summary.index,
         'Total Duration (hours)': daily_summary_hours,
-        'Total Duration (H:M:S)': daily_summary_hms
+        'Total Duration (H:M:S)': daily_summary_hms,
+        'Percentage': daily_summary_hours  # Calculate percentage
     })
     summary_df.to_markdown('./log/0summary.md', index=False)
-    return summary_df
+    
+    # Calculate global total duration
+    global_total_duration = daily_summary.sum()
+    
+    return summary_df, global_total_duration
 
-def visualize_time_data(daily_summary):
-    daily_summary.set_index('Date')['Total Duration (hours)'].plot(kind='bar')
+def visualize_time_data(daily_summary, global_total_duration):
+    ax = daily_summary.set_index('Date')['Total Duration (hours)'].plot(kind='bar')
+    for i, v in enumerate(daily_summary['Percentage']):
+        ax.text(i, daily_summary['Total Duration (hours)'][i] + 0.1, f"{v}", ha='center')
+    
+    # Calculate the second column of the title
+    start_date = datetime(2024, 7, 7)
+    end_date = datetime(2029, 7, 7)
+    current_date = datetime.now()
+    total_days = (end_date - start_date).days
+    elapsed_days = (current_date - start_date).days
+    percentage_time = (elapsed_days / total_days) * 100
+    
     plt.xlabel('Date')
     plt.ylabel('Total Duration (hours)')
-    plt.title('Total Duration by Day')
+    plt.title(f'Process of becoming a master: {global_total_duration*100 / 10000:.2f}% \n Time Progress: {percentage_time:.2f}%')
     plt.show()
 
 if __name__ == "__main__":
     log_directory = './log/'
     data = read_log_files(log_directory)
-    daily_summary = summarize_daily_durations(data)
-    visualize_time_data(daily_summary)
+    daily_summary, global_total_duration = summarize_daily_durations(data)
+    visualize_time_data(daily_summary, global_total_duration)
